c300aa2095513a00206337d54fcf05196a786a68
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2019-10-24 22:02:57 +0200

    Mise en place de l'ajout et suppression de taches

---
+++ ressources/javascript/main.js
@@ -0,0 +1,9 @@
+// User clicked on the add button
+// If there is any text inside th item field, add that text to the todo list
+document.getElementById('add').addEventListener('click', function() {
+	var value = coument.getElementById('item').value;
+	if (value) {
+		
+	}
+	console.log(value);
+});
