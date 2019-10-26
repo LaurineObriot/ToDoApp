9fc845386aeacfc3ff41b55272116e02b8a662a5
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2019-10-26 17:31:32 +0200

    Miss modif des taches

--- ressources/javascript/main.js
+++ ressources/javascript/main.js
@@ -1,5 +1,5 @@
 
-var data = {
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
 	todo: [];
 	completed: [];
 };
@@ -8,21 +8,63 @@ var data = {
 var removeSVG = //Image de la petite poubelle
 var completeSVG = //Image du petit plus
 
+renderTodoList();
+
 // User clicked on the add button
 // If there is any text inside th item field, add that text to the todo list
 document.getElementById('add').addEventListener('click', function() {
 	var value = document.getElementById('item').value;
 	if (value) {
-		addItemTodo(value);
-		document.getElementById('item').value = '';
-
-		data.todo.push(value);
+		addItem(value);
 	}
 });
+
+document.getElementById('item').addEventListener('keydown', function (e) {
+	var value = this.value;
+	if (e.code === 'Enter' && value) {
+		addItem(value);
+	}
+});
+
+fonction addItem(value) {
+	AddItemToDOM(value);
+	document.getElementById('item').value = '';
+
+	data.todo.push(value);
+	dataObjectUpdated();
+}
+
+function renderTodoList() {
+	if (!data.todo.lenght && !data.completed.lenght) return;
+
+	for (var = 0; i < data.todo.length; i++) {
+		var value = data.todo[i];
+		AddItemToDOM(value);
+	}
+
+	for (var j = 0; j < data.completed.length; j++) {
+		var value = data.completed[i];
+		AddItemToDOM(value, true);
+	}
+}
+
+function dataObjectUpdated() {
+	localStorage.setItem('todoList', JSON.stringify(data));
+}
+
 
 function removeItem() {
 	var item = this.parentNode.parentNode;
 	var parent = item.parentNode;
+	var id = parent.id;
+	var value = item.innerText;
+
+	if (id === 'todo') {
+	data.todo.splice(data.todo.index0f(value), 1);
+	} else {
+	data.completed.splice(data.completed.index0f(value), 1);
+	}
+	dataObjectUpdated();
 
 	parent.removeChild(item);
 }
@@ -31,9 +73,16 @@ function completeItem() {
 	var item = this.parentNode.parentNode;
 	var parent = item.parentNode;
 	var parentID = parent.id;
-	var value = item.innerText
+	var value = item.innerText;
 
-	data.todo.splice(data.todo.index0f(item.innerText))
+	if (id === 'todo') {
+	data.todo.splice(data.todo.index0f(value), 1);
+	data.completed.push(value);
+	} else {
+	data.completed.splice(data.completed.index0f(value), 1);
+	data.todo.push(value);
+	}
+	dataObjectUpdated();
 
 	// Check if the item should be added to the completed list or to re-added to the todo list
 	var target = (id === 'todo') ?  document.getElementById('completed') :  document.getElementById('todo')
@@ -43,9 +92,9 @@ function completeItem() {
 }
 
 // Adds a new item to the todo list
-function addItemTodo(item) {
+function AddItemToDOM(text, completed) {
 
-	var list = document.getElementById('todo');
+	var list =(completed) ? document.getElementById('completed') : document.getElementById('todo');
 
 	var item = document.createElement('li');
 	item.innerText = text;
